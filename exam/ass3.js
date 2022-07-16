108d27862619c28206add626aef56080cf334db6
Vahab7Maner <vahab7maner@gmail.com>
2022-07-16 20:21:15 +0530

    express file uploaded

--- exam/ass3.js
+++ exam/ass3.js
@@ -1,10 +1,14 @@
-var v=require('http');
+var h=require('http');
+var s=require('url');
 
-var server=v.createServer(function(req,res){
-
-	console.log("Hello from Node-JS");
-
+var server=h.createServer(function(req,res){
+ var urla=s.parse(req.url,true);
+ var name=urla.query.uid;
+ res.writeHead(200,{'content-type':'text/html'});
+ res.write("Welcome to nodejs");
+ res.end();
 });
 
-server.listen(9000,function(){});
-
+server.listen(9000,function(){
+console.log("Server started");
+});
